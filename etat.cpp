66dc5c6d37a5c3102e2415d654c59dc6eaa46667
corentinbranchereau <branchereaucorentin@gmail.com>
2021-02-23 09:44:46 +0100

    ajout des etats de 0 à 4

--- etat.cpp
+++ etat.cpp
@@ -10,30 +10,123 @@
 //---------------------------------------------------------------- INCLUDE
 
 //-------------------------------------------------------- Include système
-
+#include <iostream>
 //------------------------------------------------------ Include personnel
 #include "etat.h"
+#include "automate.h"
 //----------------------------------------------------------------- PUBLIC
 
 //----------------------------------------------------- Méthodes publiques
 bool Etat0::transition(Automate &automate, Symbole *s) {
+	switch(*s) {
+		case INT:
+			automate.decalage(s, new Etat3());
+			break;
 
+		case OPENPAR:
+			automate.decalage(s,new Etat2());
+			break;
+
+		case EXPR:
+			automate.decalage(s,new Etat1());
+			break;
+		
+		default:
+			cout << "erreur etat0" << endl;
+			break;
+    }
+	return false;
 }
 
 bool Etat1::transition(Automate &automate, Symbole *s) {
+	switch(*s) {
+		case PLUS:
+			automate.decalage(s, new Etat4());
+			break;
 
+		case MULT:
+			automate.decalage(s,new Etat5());
+			break;
+
+		case FIN:
+			return true;
+			break;
+
+		case EXPR:
+			automate.decalage(s,new Etat1());
+			break;
+		
+		default:
+			cout << "erreur etat1" << endl;
+			break;
+	}
+
+	return false;  
 }
 
 bool Etat2::transition(Automate &automate, Symbole *s) {
+	switch(*s) {
+		case INT:
+			automate.decalage(s, new Etat3());
+			break;
 
+		case OPENPAR:
+			automate.decalage(s,new Etat2());
+			break;
+
+		case EXPR:
+			automate.decalage(s,new Etat6());
+			break;
+		
+		default:
+			cout << "erreur etat2" << endl;
+			break;
+	}
+
+	return false;
 }
 
 bool Etat3::transition(Automate &automate, Symbole *s) {
+	switch(*s) {
+		case PLUS:
+		case MULT:
+		case CLOSEPAR:
+		case FIN:
+			automate.reduction(3, s);
+			break;
 
+		case EXPR:
+			automate.decalage(s,new Etat6());
+			break;
+		
+		default:
+			cout << "erreur etat3" << endl;
+			break;
+	}
+
+	return false;
 }
 
 bool Etat4::transition(Automate &automate, Symbole *s) {
+	switch(*s) {
+		case INT:
+			automate.decalage(s,new Etat3());
+			break;
 
+		case OPENPAR:
+			automate.decalage(s,new Etat2());
+			break;
+
+		case EXPR:
+			automate.decalage(s,new Etat6());
+			break;
+			
+		default:
+			cout << "erreur etat3" << endl;
+			break;
+	}
+
+	return false;
 }
 
 bool Etat5::transition(Automate &automate, Symbole *s) {
